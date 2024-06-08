ec166ebc3874f96fdad63efa410eb583a1da2fc0
Jan <jan.madon@gmail.com>
2024-06-08 18:07:47 +0200

    expanded custome request

--- AIDevs/CustomRequest.php
+++ AIDevs/CustomRequest.php
@@ -5,6 +5,8 @@ namespace app\AIDevs;
 class CustomRequest
 {
 
+    private $setopt;
+
     public function __construct(
         public string $url = "https://tasks.aidevs.pl",
     ) {
@@ -13,7 +15,11 @@ class CustomRequest
 
     public function post($endpoint, $payload, $isJson = false)
     {
-        $url = $this->url . '/' . $endpoint;
+        if($endpoint){
+            $url = $this->url . '/' . $endpoint;
+        } else {
+            $url = $this->url;
+        }
 
         if($isJson){
             $payload = json_encode($payload);
@@ -24,7 +30,12 @@ class CustomRequest
 
     public function get($endpoint)
     {
-        $url = $this->url . '/' . $endpoint;
+        if($endpoint){
+            $url = $this->url . '/' . $endpoint;
+        } else {
+            $url = $this->url;
+        }
+
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -34,34 +45,60 @@ class CustomRequest
         return $this->makeRequest($url, "get");
     }
 
+    public function addSetopt(array $setopt)
+    {
+        $this->setopt = $setopt;
+    } 
+
     private function makeRequest(string $url, string $method, array|string $payload = '', $isJson = false)
     {
+        try{
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            if($this->setopt){
+                foreach($this->setopt as $key => $value){
+                    curl_setopt($curl, $key, $value);
+                }
+            }
 
-        if($isJson){
-            curl_setopt($curl, CURLOPT_HTTPHEADER, [
-                'Content-Type: application/json',
-            ]); 
-        } else {
-            curl_setopt($curl, CURLOPT_HTTPHEADER, [
-                'Content-Type: multipart/form-data',
-            ]);
+            if($isJson){
+                curl_setopt($curl, CURLOPT_HTTPHEADER, [
+                    'Content-Type: application/json',
+                ]); 
+            } else {
+                curl_setopt($curl, CURLOPT_HTTPHEADER, [
+                    'Content-Type: multipart/form-data',
+                ]);
+            }
+
+            if ($method === 'post') {
+                curl_setopt($curl, CURLOPT_POST, true);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+            }
+
+            $executed = curl_exec($curl);
+            if(curl_error($curl)){
+                print_r('ERROR: '.curl_error($curl));    
+            };
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE); 
+            curl_close($curl);
+
+            if(json_decode($executed)){
+               return json_decode($executed);
+            }
+
+            if($httpCode === 500){
+                return false;
+            }
+
+            return $executed;
+        } catch(\Exception $error) {
+            print_r($error->getMessage());
         }
-
-        if ($method === 'post') {
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
-        }
-
-        if(curl_exec($curl)){
-            //error handling and log    
-        };
-
-        curl_close($curl);
-        $response = json_decode(curl_exec($curl));
-        return $response;
+        
     }
+
+    
 }
